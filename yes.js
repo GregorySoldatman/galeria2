c9890f4623a9d24b18ffb37bc4cc379b83856dfa
GregorySoldatman <grzegorzwojakowski0@gmail.com>
2023-03-13 12:55:06 +0100

    Setup do testów

--- yes.js
+++ yes.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+const testButton = document.querySelector('.open-test')
+const testModal = document.querySelector('#modal1')
+
+testButton.addEventListener('click', () => {
+    testModal.style.removeProperty('display')
+    setTimeout(() => {testModal.classList.add('active')}, 1)
+})
